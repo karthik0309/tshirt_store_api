67c3d3b4be9ca8e829a5d5f3f2c5031e4f996972
Karthik Belida <Karthik392001@gmail.com>
2022-12-27 12:00:35 +0530

    Initial commit

---
+++ controllers/paymentb.js
@@ -0,0 +1,42 @@
+var braintree = require("braintree");
+require("dotenv").config();
+
+var gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.MERCHANTID,
+  publicKey: process.env.PUBLICKEY,
+  privateKey: process.env.PRIVATEKEY,
+});
+
+exports.getToken = (req, res) => {
+  gateway.clientToken.generate({}, function(err, response) {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      res.send(response);
+    }
+  });
+};
+
+exports.processPayment = (req, res) => {
+  let nonceFromTheClient = req.body.paymentMethodNonce;
+
+  let amountFromTheClient = req.body.amount;
+  gateway.transaction.sale(
+    {
+      amount: amountFromTheClient,
+      paymentMethodNonce: nonceFromTheClient,
+
+      options: {
+        submitForSettlement: true
+      }
+    },
+    function(err, result) {
+      if (err) {
+        res.status(500).json(error);
+      } else {
+        res.json(result);
+      }
+    }
+  );
+};
